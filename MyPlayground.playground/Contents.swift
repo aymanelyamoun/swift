d448286ea68537416910bb1efd61a83c842cfee3
aymane <elyamoun>
2022-05-25 22:55:43 +0100

    fist swift practice

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+let expectedMinutesInOven:Int;
+
+expectedMinutesInOven = 40;
+func remainingMinutesInOven(elapsedMinutes:Int) -> Int
+{
+    return (expectedMinutesInOven - elapsedMinutes);
+}
+
+func preparationTimeInMinutes(layers:Int) ->Int
+{
+    return (layers * 2);
+}
+
+func totalTimeInMinutes(layers:Int, elapsedMinutes:Int) -> Int
+{
+    return (elapsedMinutes + preparationTimeInMinutes(layers: layers));
+}
